1a4bad89f711fb3243c1a93bba5d9fd278b2e77e
Courtship_fy <courtship_fy@courtship.top>
2026-05-19 17:34:50 +0800

    新增打字机效果及初级配置

--- NarrRail/Source/NarrRail/NarrRail.Build.cs
+++ NarrRail/Source/NarrRail/NarrRail.Build.cs
@@ -10,7 +10,8 @@ public class NarrRail : ModuleRules
         {
             "Core",
             "CoreUObject",
-            "Engine"
+            "Engine",
+            "DeveloperSettings"
         });
 
         PrivateDependencyModuleNames.AddRange(new[]
